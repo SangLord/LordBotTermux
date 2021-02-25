660fd87903fad544a4feb2bfe73ac2baeb6e4306
SangLord <79593966+SangLord@users.noreply.github.com>
2021-02-25 14:21:33 +0700

    Add files via upload

---
+++ SANGLORD/listmenu.js
@@ -0,0 +1,264 @@
+const listmenu = (prefix) => { 
+	return ` *LIST MENU FADHIL BOT*
+	
+	
+╭─「 *MEDIA DOWNLOADER* 」──
+├➲ *${prefix}tiktokstalk [username]*
+├➲ *${prefix}igstalk [ahmd.fdhl_]*
+├➲ *${prefix}insta [Link]*
+├➲ *${prefix}instastory [username]*
+├➲ *${prefix}ssweb [url]*
+├➲ *${prefix}url2img [Url]*
+├➲ *${prefix}tiktok*
+├➲ *${prefix}fototiktok*
+├➲ *${prefix}meme*
+├➲ *${prefix}memeindo*
+├➲ *${prefix}kbbi*
+├➲ *${prefix}wait*
+├➲ *${prefix}trendtwit*
+├➲ *${prefix}google [berita terkini]*
+├➲ *${prefix}pinterest*
+├➲ *${prefix}play*
+╰─────────────────────
+
+╭────「 *FUN & GAME* 」────
+├➲ *${prefix}anjing*
+├➲ *${prefix}kucing*
+├➲ *${prefix}testime*
+├➲ *${prefix}hilih*
+├➲ *${prefix}say*
+├➲ *${prefix}apakah*
+├➲ *${prefix}kapankah*
+├➲ *${prefix}bisakah*
+├➲ *${prefix}rate*
+├➲ *${prefix}watak*
+├➲ *${prefix}hobby*
+├➲ *${prefix}infogempa*
+├➲ *${prefix}infonomor*
+├➲ *${prefix}quotes*
+├➲ *${prefix}truth*
+├➲ *${prefix}dare*
+├➲ *${prefix}katabijak*
+├➲ *${prefix}fakta*
+├➲ *${prefix}darkjokes*
+├➲ *${prefix}bucin*
+├➲ *${prefix}pantun*
+├➲ *${prefix}katacinta*
+├➲ *${prefix}jadwaltvnow*
+├➲ *${prefix}hekerbucin*
+├➲ *${prefix}katailham*
+├➲ *${prefix}caklontong*
+├➲ *${prefix}family100*
+╰─────────────────────
+
+
+╭─────────────────────
+├➲ *${prefix}asupan*
+├➲ *${prefix}tebakgambar*
+├➲ *${prefix}caklontong*
+├➲ *${prefix}family100*
+├➲ *${prefix}kalkulator [13*12]*
+├➲ *${prefix}wp [gunung]*
+╰─────────────────────
+
+
+╭───「 *OTHER & DEFACE* 」───
+├➲ *${prefix}jarak [Jombang/Jakarta]*
+├➲ *${prefix}translate [en/Apa kabar?]*
+├➲ *${prefix}pasangan [Udin/Uden]*
+├➲ *${prefix}gantengcek [I AM]*
+├➲ *${prefix}cantikcek [TEXT]*
+├➲ *${prefix}artinama [NAMA LU]*
+├➲ *${prefix}persengay [NAMA LU]*
+├➲ *${prefix}pbucin [SANG LORD]*
+├➲ *${prefix}bpfont [SANG LORD]*
+├➲ *${prefix}textstyle [SANG × LORD]*
+├➲ *${prefix}jadwaltv [GTV]*
+├➲ *${prefix}lirik [Sasageyo]*
+├➲ *${prefix}chord [Sasageyo]*
+├➲ *${prefix}wiki [Adolf Hitler]*
+├➲ *${prefix}brainly [pertanyaan]*
+├➲ *${prefix}resepmasakan [Pecel]*
+├➲ *${prefix}map [Jombang]*
+├➲ *${prefix}film [The Walking Death]*
+├➲ *${prefix}pinterest [gambar kucing]*
+├➲ *${prefix}infocuaca [Jombang]*
+├➲ *${prefix}jamdunia [Indonesia]*
+├➲ *${prefix}mimpi [Sultan Pree peler]*
+├➲ *${prefix}infoalamat [jalan Jombang]*
+╰─────────────────────
+
+
+╭─────────────────────
+├➲ *${prefix}asupan*
+├➲ *${prefix}tebakgambar*
+├➲ *${prefix}caklontong*
+├➲ *${prefix}family100*
+├➲ *${prefix}kalkulator [13*12]*
+├➲ *${prefix}wp [gunung]*
+╰─────────────────────
+
+╭─────────────────────
+├➲ *${prefix}jadwalsholat [Jombang]*
+├➲ *${prefix}quran*
+├➲ *${prefix}quransurah [1]*
+├➲ *${prefix}tafsir [1/5]*
+╰─────────────────────
+
+╭─────────────────────
+├➲ *${prefix}becrypt [string]*
+├➲ *${prefix}encode64 [string]*
+├➲ *${prefix}decode64 [encrypt]*
+├➲ *${prefix}encode32 [string]*
+├➲ *${prefix}decode32 [encrypt]*
+├➲ *${prefix}encbinary [string]*
+├➲ *${prefix}decbinary [encrypt]*
+├➲ *${prefix}encoctal [string]*
+├➲ *${prefix}decoctal [encrypt]*
+├➲ *${prefix}hashidentifier [Encrypt Hash]*
+├➲ *${prefix}dorking [dork]*
+├➲ *${prefix}pastebin [teks]*
+├➲ *${prefix}tinyurl [link]*
+├➲ *${prefix}bitly [link]*
+╰─────────────────────
+
+╭───「 *CREATOR MENU* 」───
+├➲ *${prefix}sticker*
+├➲ *${prefix}quotemaker [tx/wtrmk/tema]*
+├➲ *${prefix}nulis [nama/kelas/text]*
+├➲ *${prefix}trigger [reply image]*
+├➲ *${prefix}rip [reply image]*
+├➲ *${prefix}wasted [reply image]*
+├➲ *${prefix}cphlogo [Sang/Lord]*
+├➲ *${prefix}cglitch [Sang/Lord]*
+├➲ *${prefix}cpubg [Sang/Lord]*
+├➲ *${prefix}cml [Sang/Lord]*
+├➲ *${prefix}tahta [29]*
+├➲ *${prefix}croman [SangLord dan 29]*
+├➲ *${prefix}cthunder [Sang Lord]*
+├➲ *${prefix}cbpink [Sang Lord]*
+├➲ *${prefix}cmwolf [Sang Lord]*
+├➲ *${prefix}csky [Sang Lord]*
+├➲ *${prefix}cwooden [Sang Lord]*
+├➲ *${prefix}cflower [Sang Lord]*
+├➲ *${prefix}clove [Sang Lord]*
+├➲ *${prefix}ccrossfire [Sang Lord]*
+├➲ *${prefix}cnaruto [Sang Lord]*
+├➲ *${prefix}cparty [Sang Lord]*
+├➲ *${prefix}cshadow [Sang Lord]*
+├➲ *${prefix}cminion [Sang Lord]*
+├➲ *${prefix}cneon [Sang Lord]*
+├➲ *${prefix}cneon2 [Sang Lord]*
+├➲ *${prefix}cneongreen [Sang Lord]*
+├➲ *${prefix}c3d [Sang Lord]*
+├➲ *${prefix}csky [Sang Lord]*
+├➲ *${prefix}tts [id SangLord Gans]*
+├➲ *${prefix}ttp [Sang Lord]*
+├➲ *${prefix}slide [Sang Lord]*
+├➲ *${prefix}stiker*
+├➲ *${prefix}gifstiker*
+├➲ *${prefix}toimg*
+├➲ *${prefix}img2url*
+├➲ *${prefix}nobg*
+├➲ *${prefix}tomp3*
+├➲ *${prefix}ocr*
+╰──────────────────────
+
+╭──────「 *GROUP ONLY* 」───
+├➲ *${prefix}modeanime [On/Off]*
+├➲ *${prefix}naruto*
+├➲ *${prefix}minato*
+├➲ *${prefix}boruto*
+├➲ *${prefix}hinata*
+├➲ *${prefix}sakura*
+├➲ *${prefix}sasuke*
+├➲ *${prefix}kaneki*
+├➲ *${prefix}toukachan*
+├➲ *${prefix}rize*
+├➲ *${prefix}akira*
+├➲ *${prefix}itori*
+├➲ *${prefix}kurumi*
+├➲ *${prefix}miku*
+├➲ *${prefix}anime*
+├➲ *${prefix}animecry*
+├➲ *${prefix}neonime*
+├➲ *${prefix}animekiss*
+├➲ *${prefix}wink*
+╰─────────────────────
+
+╭─────────────────────
+├➲ *${prefix}welcome [On/Off]*
+├➲ *${prefix}grup [buka/tutup]*
+├➲ *${prefix}antilink [on/off*
+├➲ *${prefix}ownergrup*
+├➲ *${prefix}setpp*
+├➲ *${prefix}infogc*
+├➲ *${prefix}add*
+├➲ *${prefix}kick*
+├➲ *${prefix}promote*
+├➲ *${prefix}demote*
+├➲ *${prefix}setname*
+├➲ *${prefix}setdesc*
+├➲ *${prefix}linkgrup*
+├➲ *${prefix}tagme*
+├➲ *${prefix}hidetag*
+├➲ *${prefix}tagall*
+├➲ *${prefix}mentionall*
+├➲ *${prefix}fitnah*
+├➲ *${prefix}listadmin*
+├➲ *${prefix}openanime*
+├➲ *${prefix}edotense*
+╰──────────────────────
+
+╭──────────────────────
+├➲ *${prefix}nsfw [On/Off]*
+├➲ *${prefix}nsfwloli*
+├➲ *${prefix}nsfwblowjob*
+├➲ *${prefix}nsfwneko*
+├➲ *${prefix}nsfwtrap*
+├➲ *${prefix}hentai*
+├➲ *${prefix}simih [On/Off]*
+╰──────────────────────
+
+╭────「 *OWNER ONLY* 」─────
+├➲ *${prefix}addprem [mentioned]*
+├➲ *${prefix}removeprem [mention]*
+├➲ *${prefix}setppbot*
+├➲ *${prefix}setreply*
+├➲ *${prefix}bc*
+├➲ *${prefix}bcgc*
+├➲ *${prefix}ban*
+├➲ *${prefix}unban*
+├➲ *${prefix}block*
+├➲ *${prefix}unblock*
+├➲ *${prefix}clearall*
+├➲ *${prefix}delete*
+├➲ *${prefix}clone*
+├➲ *${prefix}getses*
+├➲ *${prefix}leave*
+╰──────────────────────
+
+
+╭─────「 *PREMIUM ONLY* 」───
+├➲ *${prefix}playmp3 [I am Lady]*
+├➲ *${prefix}fb [link video]*
+├➲ *${prefix}snack [link snack video]*
+├➲ *${prefix}ytmp3 [link yt]*
+├➲ *${prefix}ytmp4 [link yt]*
+├➲ *${prefix}joox [Monolog Pamungkas]*
+├➲ *${prefix}smule [Link Video Smule]*
+├➲ *${prefix}cersex*
+├➲ *${prefix}asupan*
+├➲ *${prefix}xxx [japan]*
+├➲ *${prefix}pornhub [GangBang]*
+├➲ *${prefix}hentai [Random]*
+├➲ *${prefix}ban [TagUser]*
+├➲ *${prefix}unban [TagUser]*
+├➲ *${prefix}bc [teks]*
+├➲ *${prefix}asupan*
+╰──────────────────────
+
+	
+               *©SangLord*`
+	}
+exports.listmenu = listmenu
---
+++ SANGLORD/makermenu.js
@@ -0,0 +1,49 @@
+const makermenu = (prefix) => { 
+	return ` 
+╭───「 *CREATOR MENU* 」───
+│
+├➲ *${prefix}sticker*
+├➲ *${prefix}quotemaker [tx/wtrmk/tema]*
+├➲ *${prefix}nulis [nama/kelas/text]*
+├➲ *${prefix}trigger [reply image]*
+├➲ *${prefix}rip [reply image]*
+├➲ *${prefix}wasted [reply image]*
+├➲ *${prefix}cphlogo [Sang/Lord]*
+├➲ *${prefix}cglitch [Sang/Lord]*
+├➲ *${prefix}cpubg [Sang/Lord]*
+├➲ *${prefix}cml [Sang/Lord]*
+├➲ *${prefix}tahta [29]*
+├➲ *${prefix}croman [Sang dan Lord]*
+├➲ *${prefix}cthunder [SangLord]*
+├➲ *${prefix}cbpink [SangLord]*
+├➲ *${prefix}cmwolf [SangLord]*
+├➲ *${prefix}csky [I AM GANS]*
+├➲ *${prefix}cwooden [SangLord]*
+├➲ *${prefix}cflower [LordGans]*
+├➲ *${prefix}clove [SangLord]*
+├➲ *${prefix}ccrossfire [Sanglord]*
+├➲ *${prefix}cnaruto [SangLord]*
+├➲ *${prefix}cparty [SangLord]*
+├➲ *${prefix}cshadow [SangLord]*
+├➲ *${prefix}cminion [SangLord]*
+├➲ *${prefix}cneon [SangLord]*
+├➲ *${prefix}cneon2 [SangLord]*
+├➲ *${prefix}cneongreen [SangLord]*
+├➲ *${prefix}c3d [SangLord]*
+├➲ *${prefix}csky [SangLord]*
+├➲ *${prefix}tts [id I AM GANS]*
+├➲ *${prefix}ttp [SangLord]*
+├➲ *${prefix}slide [SangLord]*
+├➲ *${prefix}stiker*
+├➲ *${prefix}gifstiker*
+├➲ *${prefix}toimg*
+├➲ *${prefix}img2url*
+├➲ *${prefix}nobg*
+├➲ *${prefix}tomp3*
+├➲ *${prefix}ocr*
+│
+╰──────────────────────
+
+	           *©Sang Lord*`
+	}
+exports.makermenu = makermenu
